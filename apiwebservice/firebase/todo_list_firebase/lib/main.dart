e7b2489111331997fd6dc6096f5f177d11f9a8ed
leoALXHIl <leoaleixohilario@gmail.com>
2025-09-04 11:27:09 -0300

    04.09.2025 Firebase

---
+++ apiwebservice/firebase/todo_list_firebase/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_list_firebase/firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized(); //garantir a inicialização dos bindings
+  // inicializa o firebase - ao mesmo tempo que abre as telas
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(
+    title: "Lista de Tarefas", //direciona para a tela de autenticação
+    home: AutenticacaoView( 
+
+    ),
+  ));
+  }
